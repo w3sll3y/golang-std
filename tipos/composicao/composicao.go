0859752de080ca54ef39564a85254008499e6485
Wesley Fernandes <fernandess.weslley@gmail.com>
2024-01-10 12:42:58 -0300

    types

---
+++ tipos/composicao/composicao.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type esportivo interface {
+	ligarTurbo()
+}
+
+type luxuoso interface {
+	fazerBaliza()
+}
+
+type esportivoLuxuoso interface {
+	esportivo
+	luxuoso
+	// pode adicionar mais metodos
+}
+
+type bmw7 struct{}
+
+func (b bmw7) ligarTurbo() {
+	fmt.Println("Turbo...")
+}
+
+func (b bmw7) fazerBaliza() {
+	fmt.Println("Balizaa...")
+}
+
+func main() {
+	var b esportivoLuxuoso = bmw7{}
+	b.ligarTurbo()
+	b.fazerBaliza()
+}
